0e603e0357562068837596b266fc133b29b8099a
ZachHamiton <zach.hamilton@kingsway.school.nz>
2025-07-17 18:49:12 +1200

    Made Player Movement.

--- Term 3 Assesment game/main.py
+++ Term 3 Assesment game/main.py
@@ -3,19 +3,66 @@
 # Date: 15/07/2025 
 # Version 1 
 
-# test
-
-
-
 # imports
 import pygame
 from sys import exit
+
+
+class Player (pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.player_image = pygame.image.load("images/player_green.png").convert_alpha()
+
+        self.image = self.player_image
+        self.rect =  self.player_image.get_rect(midbottom = (600,400))
+
+
+        self.gravity = 0 
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.y >= 400:
+            self.rect.y = 400
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > 1130:
+            self.rect.x = 1130
+
+    
+    
+    def player_imput(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]:
+            if self.rect.y >= 400:
+                self.gravity = -20
+
+
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if self.rect.y == 400:
+                self.rect.x += 5
+            else:
+                self.rect.x += 8
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if self.rect.y == 400:
+                self.rect.x -= 5
+            else:
+                self.rect.x -= 8
+
+    def update(self):
+        self.apply_gravity()
+        self.player_imput()
+
+
+        
 
 # starts pygame
 pygame.init() 
 
 # Creates the play window
-screen = pygame.display.set_mode((1200,600))
+screen = pygame.display.set_mode((1200,675))
 
 # sets the title for the game.
 pygame.display.set_caption("Ultimate Pygame")
@@ -28,10 +75,17 @@ pygame.display.set_caption("Crossing the Deep")
 
 FPS = 60
 
+# Groups
+# add player to group
+player =  pygame.sprite.GroupSingle()
+player.add(Player())
+
+
 # images
-background_surf = pygame.image.load("images/stormy_background.png")
-
-
+background_surf = pygame.image.load("images/background_frame_5(copy)(Medium).png").convert_alpha()
+# ship_surf = pygame.image.load("images/ship.png").convert_alpha()
+# ship_surf = pygame.transform.rotozoom(ship_surf,0,1.2)
+# ship_rect = ship_surf.get_rect(midbottom = (600,725))
 
 while True:
 
@@ -43,13 +97,15 @@ while True:
             exit()
 
 
-    screen.blit(background_surf,(0,-150))
-    
+    screen.blit(background_surf,(0,0))
+    # screen.blit(ship_surf, ship_rect)
 
 
     # updates pygame display.
+    player.draw(screen)
+    player.update()
     pygame.display.update()
-
+    
     # While loop can only run at FPS speed per second. 
     clock.tick(FPS)
 
