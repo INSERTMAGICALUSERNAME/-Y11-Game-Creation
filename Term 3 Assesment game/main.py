e27a1e9e373d71898cec5047ef9443cb3a386e85
ZachHamiton <zach.hamilton@kingsway.school.nz>
2025-07-21 20:10:53 +1200

    Made Breakage class and breakage spawning.

--- Term 3 Assesment game/main.py
+++ Term 3 Assesment game/main.py
@@ -6,6 +6,7 @@
 # imports
 import pygame
 from sys import exit
+import random
 
 # player class
 class Player (pygame.sprite.Sprite):
@@ -61,6 +62,29 @@ class Player (pygame.sprite.Sprite):
         self.player_imput()
 
 
+
+class Breakage(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+
+        breakage_image = pygame.image.load("images/breakage.png").convert_alpha()
+        self.image = breakage_image
+        self.rect = self.image.get_rect(midbottom = (600, 400))
+        
+        if type == 'sail':
+            self.y_pos = 400
+            self.x_pos = 600
+            self.rect = self.image.get_rect(midtop = (self.x_pos, self.y_pos))
+        elif type == 'hull':
+            self.y_pos = 400
+            self.x_pos = 200
+            self.rect = self.image.get_rect(midtop = (self.x_pos, self.y_pos))
+        else:
+            self.y_pos = 400
+            self.x_pos = 1000
+            self.rect = self.image.get_rect(midtop = (self.x_pos, self.y_pos))
+
+
         
 
 # starts pygame
@@ -88,6 +112,10 @@ FPS = 60
 player =  pygame.sprite.GroupSingle()
 player.add(Player())
 
+# breakage group
+breakage = pygame.sprite.Group()
+
+
 #text
 
 #title
@@ -95,18 +123,22 @@ title_surf = pacific_font.render('Pacific Pursuit', True,"#006439")
 title_rec = title_surf.get_rect(center = (600,373))
 
 #blurb
-
 info_surf = pacific_font.render('Press Space to Start, Press esacpe to quit, Press "h" for how to play',True,('#006439'))
 info_surf = pygame.transform.scale(info_surf,(1000,50))
 info_surf_rec = info_surf.get_rect(midbottom = (600, 500))
 
-
-
+# set breakages spawning lists
+breakage_type_eligible_list = ['sail', 'hull', 'mast']
+breakage_type_ineligible_list = []
 
 # images
-background_surf = pygame.image.load("images/background_frame_5(copy)(Medium).png").convert_alpha()
+background_surf = pygame.image.load("images/stormy_background(Medium).png").convert_alpha()
 game_state = 2 
 
+
+# clocks
+breakage_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(breakage_timer,1000)
 
 while True:
 
@@ -125,33 +157,46 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 exit()
+        if game_state == 1:
+
+            # Breakages spawning. Gets a random item from a list. adds it to the breakages sprite class, removes from current list and than put on another list. 
+            if event.type == breakage_timer:
+                if breakage_type_eligible_list:
+                    removed_breakage = breakage_type_eligible_list[random.randint(0, len(breakage_type_eligible_list) - 1)]
+                    breakage.add(Breakage(removed_breakage))
+                    breakage_type_eligible_list.remove(removed_breakage)
+                    breakage_type_ineligible_list.append(removed_breakage) # might not be needed for use laster
+                
+
+
+                    
 
        
-            
-            
-            
-            
-            
-                
 
     #main gameplay
     if game_state == 1:
-        screen.blit(background_surf,(0,-150))
+        screen.blit(background_surf,(0,0))
+
         # updates pygame display.
+
+        # drawing the breakage
+        breakage.draw(screen)
+        
+        # drawing the player
         player.draw(screen)
         player.update()
     
     #title and game over screen
     if game_state == 2:
 
-        screen.blit(background_surf,(0,-150))
+        screen.blit(background_surf,(0,0))
         pygame.draw.rect(screen,"#673506FF",(50,300,1100,300))
         pygame.draw.rect(screen,"#2C2C2CCC",(50,300,1100,300),10,2)
         screen.blit(info_surf,info_surf_rec)
         screen.blit(title_surf,title_rec)
         
     if game_state == 3:
-        screen.blit(background_surf,(0,-150))
+        screen.blit(background_surf,(0,0))
         pygame.draw.rect(screen,"#673506FF",(50,25,1100,625))
         pygame.draw.rect(screen,"#2C2C2CCC",(50,25,1100,625),10,2)
         
