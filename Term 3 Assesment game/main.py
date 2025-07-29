1c220cc8bd9906171a10cf8c03bdff9098f8396f
INSERTMAGICALUSERNAME <Johann.shotbolt@kingsway.school.nz>
2025-07-29 12:58:01 +1200

    Added compass bar and button functionality

--- Term 3 Assesment game/main.py
+++ Term 3 Assesment game/main.py
@@ -57,7 +57,55 @@ class Player (pygame.sprite.Sprite):
         self.player_imput()
 
 
+class Button(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        self.type = type
+
+        self.image = pygame.image.load("images/Wooden_plank.png").convert_alpha()
         
+
+        self.x_pos = 600
+        if type == 'top':
+            self.y_pos = 280
+            
+        if type == 'middle':
+            self.y_pos = 372
+            
+        if type == 'bottom':
+            self.y_pos = 464
+        
+        
+    
+        # self.image = pygame.transform.scale(self.image,(360,65))
+        self.rect = self.image.get_rect(center = (self.x_pos, self.y_pos))
+
+    def check_click(self,mouse_pos,event):
+        clicked = None
+        if self.rect.collidepoint(mouse_pos):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if plank.type == 'top':
+                    clicked = 1
+                if plank.type == 'middle':
+                    clicked = 2
+                if plank.type == 'bottom':
+                    clicked = 3       
+        return clicked
+            
+        
+        
+        
+        
+            
+
+
+    
+    
+    def update(self):
+        self.draw(screen)
+        self.check_click(pygame.mouse.get_pos())
+        
+    
 
 # starts pygame
 pygame.init() 
@@ -69,7 +117,7 @@ screen = pygame.display.set_mode((1200,675))
 pygame.display.set_caption("Ultimate Pygame")
 
 #creating font
-pacific_font = pygame.font.Font('Font/Pacifico-regular.ttf',50)
+pacific_font = pygame.font.Font('Font/Pacifico-regular.ttf',75)
 pixel_font = pygame.font.Font('Font/Pixeltype.ttf',50)
 
 # sets the frame rate of the game.
@@ -99,23 +147,39 @@ tilt = (920 + wind_strength)
 
 
 #title
-title_surf = pixel_font.render('Pacific Pursuit', True,"#006439")
-title_rec = title_surf.get_rect(center = (600,373))
+title_surf = pacific_font.render('Pacific Pursuit', True,"#5fa8a9")
+title_rec = title_surf.get_rect(center = (600,144))
 
 plank_surf = pygame.image.load("images/Wooden_plank.png").convert_alpha()
 plank_rect = plank_surf.get_rect(center = (600, 337))
 
 hanging_sign = pygame.image.load("images/Hanging_Sign-removebg-preview.png").convert_alpha()
 hanging_sign = pygame.transform.scale(hanging_sign,(500,700))
-hanging_sign_rec = hanging_sign.get_rect(center = (600, 300))
+hanging_sign_rec = hanging_sign.get_rect(center = (600, 275))
 
 #blurb
 
-info_surf = pixel_font.render('Press Space to Start, Press esacpe to quit, Press "h" for how to play',True,('#006439'))
-info_surf = pygame.transform.scale(info_surf,(1000,50))
-info_surf_rec = info_surf.get_rect(midbottom = (600, 500))
+start_surf = pacific_font.render('START',True,("#342218"))
+start_surf = pygame.transform.scale(start_surf,(200,50))
+start_surf_rec = start_surf.get_rect(midbottom = (600, 305))
+
+how_surf = pacific_font.render('How To Play',True,("#342218"))
+how_surf = pygame.transform.scale(how_surf,(200,50))
+how_surf_rec = how_surf.get_rect(midbottom = (600, 400))
+
+quit_surf = pacific_font.render('QUIT',True,("#342218"))
+quit_surf = pygame.transform.scale(quit_surf,(200,50))
+quit_surf_rec = quit_surf.get_rect(midbottom = (600, 490))
 
 
+#buttons
+top = 'top'
+middle = 'middle'
+bottom = 'bottom'
+button = pygame.sprite.Group()
+button.add(Button(top))
+button.add(Button(middle))
+button.add(Button(bottom))
 
 
 # images
@@ -141,18 +205,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit() # oposite of pygame.init()
             exit()
+
         if game_state == 2:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_state = 1   
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_h :
-             game_state = 3
-                
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                exit()
-        if game_state == 3:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                game_state = 2
+            for plank in button:
+                mouse_pos = pygame.mouse.get_pos()
+                type_button_clicked = plank.check_click(mouse_pos,event)
+                if type_button_clicked == 1:
+                    game_state = 1
+                if type_button_clicked == 2:
+                    game_state = 3
+                if type_button_clicked == 3:
+                    pygame.quit()
+                    exit()
+            
+            
 
 
        
@@ -165,7 +231,7 @@ while True:
 
     #main gameplay
     if game_state == 1:
-        screen.blit(background_surf,(0,-150))
+        screen.blit(background_surf,(0,0))
         #compass and wind
         if wind_right:
             
@@ -189,19 +255,20 @@ while True:
             
         if tilt >= 980:
             tilt = 980
-        print(tilt)
+        
 
         if wind_strength > 180:
             wind_strength = 180
         if wind_strength < -180:
             wind_strength = -180
-        print(wind_strength)
+        
             
         compass_direction_rect.x = tilt
         
         screen.blit(compass_direction,compass_direction_rect)
         screen.blit(compass_bar,compass_bar_rect)
-        
+        pygame.draw.rect(screen,"#000000FF",(772,90,250,20))
+        pygame.draw.rect(screen,"#000000FF",(1178,90,22,20))
 
 
 
@@ -213,21 +280,22 @@ while True:
     #title and game over screen
     if game_state == 2:
 
-        screen.blit(background_surf,(0,-150))
-        pygame.draw.rect(screen,"#C59568FF",(300,20,600,575))
-        pygame.draw.rect(screen,"#3C2005FF",(300,20,600,575),10)
-        screen.blit(hanging_sign,hanging_sign_rec)
-        # screen.blit(plank_surf,plank_rect)
-        # screen.blit(plank_surf,(plank_rect.x,plank_rect.y + 100))
-        # screen.blit(plank_surf,(plank_rect.x,plank_rect.y - 100))
+        screen.blit(background_surf,(0,0))
+        pygame.draw.rect(screen,"#c3dbdb",(title_rec.x-10 ,title_rec.y+10,title_rec.width + 30,title_rec.height))
+        pygame.draw.rect(screen,"#6E6F71FF",(title_rec.x-10 ,title_rec.y+10,title_rec.width + 30,title_rec.height),10)
         
-        screen.blit(info_surf,info_surf_rec)
         screen.blit(title_surf,title_rec)
-        
+        screen.blit(hanging_sign,hanging_sign_rec)
+        button.draw(screen)
+        screen.blit(start_surf,start_surf_rec)
+        screen.blit(how_surf,how_surf_rec)
+        screen.blit(quit_surf,quit_surf_rec)
     if game_state == 3:
         screen.blit(background_surf,(0,-150))
         pygame.draw.rect(screen,"#673506FF",(50,25,1100,625))
         pygame.draw.rect(screen,"#2C2C2CCC",(50,25,1100,625),10,2)
+
+        
         
 
         
